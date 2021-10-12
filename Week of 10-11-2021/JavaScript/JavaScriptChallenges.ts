e92b49602b060e25faaa0e536b7f04ed864de450
cptvictor <victorsyanwenliu19@gmail.com>
2021-10-12 16:46:39 -0400

    challenge 1 complete

--- Week of 10-11-2021/JavaScript/JavaScriptChallenges.ts
+++ Week of 10-11-2021/JavaScript/JavaScriptChallenges.ts
@@ -1,12 +1,23 @@
 function IngWords(wordList: string[])
 {
-    let ingWords: string[];
+    let ingWords: string[] = [];
     for (const word of wordList) 
     {
         if(word.endsWith("ing"))
         {
-            ingWords.push(word);
+            let firstSyllables: string = word.substring(0, word.length-4);
+            if(firstSyllables.includes("a") 
+                || firstSyllables.includes("e") 
+                || firstSyllables.includes("i") 
+                || firstSyllables.includes("o") 
+                || firstSyllables.includes("u") 
+                || firstSyllables.includes("y"))
+            {
+                ingWords.push(word);
+            }
         }
     }
     return ingWords;
 }
+
+console.log(IngWords(["bring", "carrying", "bed", "Ace", "mooring", "apple"]));
